5731b86e68c87be37b1e7bb3c48603c03a751cd3
Salvatore Laisa <moebiusmania@gmail.com>
2018-12-19 14:46:00 +0100

    update data

--- data/data.js
+++ data/data.js
@@ -1,26 +1,128 @@
 module.exports = [
   { 
-    "name": "Babel", 
+    "name": "Kubernetes", 
     "ring": "adopt", 
-    "quadrant": "devops", "isNew": "FALSE", 
-    "description": "This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href=\"https://www.thoughtworks.com\">anchor links to documentation and referance material</a>." 
-  }, { 
-    "name": "Consul", 
+    "quadrant": "devops", 
+    "isNew": "FALSE", 
+    "description": "" 
+  }, 
+  { 
+    "name": "Openshift", 
     "ring": "trial", 
+    "quadrant": "devops", 
+    "isNew": "FALSE", 
+    "description": "" 
+  }, 
+  { 
+    "name": "Gitops", 
+    "ring": "assess", 
+    "quadrant": "devops", 
+    "isNew": "FALSE", 
+    "description": "" 
+  }, 
+  { 
+    "name": "Cloud native", 
+    "ring": "adopt", 
+    "quadrant": "devops", 
+    "isNew": "FALSE", 
+    "description": "" 
+  }, 
+  { 
+    "name": "Confluent platform", 
+    "ring": "adopt", 
+    "quadrant": "devops", 
+    "isNew": "FALSE", 
+    "description": "" 
+  }, 
+  { 
+    "name": "Scala", 
+    "ring": "adopt", 
     "quadrant": "engineering", 
     "isNew": "FALSE", 
-    "description": "This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href=\"https://www.thoughtworks.com\">anchor links to documentation and referance material</a>." 
-  }, { 
-    "name": "Grafana", 
+    "description": "" 
+  }, 
+  { 
+    "name": "Kafka", 
+    "ring": "adopt", 
+    "quadrant": "engineering", 
+    "isNew": "FALSE", 
+    "description": "" 
+  }, 
+  { 
+    "name": "Go", 
+    "ring": "assess", 
+    "quadrant": "engineering", 
+    "isNew": "FALSE", 
+    "description": "" 
+  }, 
+  { 
+    "name": "Typescript", 
     "ring": "assess", 
     "quadrant": "ui engineering", 
     "isNew": "FALSE", 
-    "description": "This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href=\"https://www.thoughtworks.com\">anchor links to documentation and referance material</a>." 
-  }, { 
-    "name": "Packer", 
+    "description": "" 
+  }, 
+  { 
+    "name": "Conversational UI", 
+    "ring": "assess", 
+    "quadrant": "ui engineering", 
+    "isNew": "FALSE", 
+    "description": "" 
+  }, 
+  { 
+    "name": "One-fits-all framework", 
+    "ring": "hold", 
+    "quadrant": "ui engineering", 
+    "isNew": "FALSE", 
+    "description": "" 
+  }, 
+  {
+    "name": "Design System",
+    "ring": "trial",
+    "quadrant": "ui engineering",
+    "isNew": "FALSE",
+    "description": ""
+  }, 
+  {
+    "name": "UI & UX first",
+    "ring": "adopt",
+    "quadrant": "ui engineering",
+    "isNew": "FALSE",
+    "description": ""
+  }, 
+  {
+    "name": "Cross framework",
+    "ring": "adopt",
+    "quadrant": "ui engineering",
+    "isNew": "FALSE",
+    "description": ""
+  }, 
+  { 
+    "name": "Event storming", 
+    "ring": "assess", 
+    "quadrant": "methodologies", 
+    "isNew": "FALSE", 
+    "description": "" 
+  },
+  { 
+    "name": "Open source", 
+    "ring": "trial", 
+    "quadrant": "methodologies", 
+    "isNew": "FALSE", 
+    "description": "" 
+  },
+  { 
+    "name": "Waterfall", 
     "ring": "hold", 
     "quadrant": "methodologies", 
     "isNew": "FALSE", 
-    "description": "This is the description. You can use basic html such as the <strong>strong tag to emphasise keywords and phrases</strong> and insert <a href=\"https://www.thoughtworks.com\">anchor links to documentation and referance material</a>." 
+    "description": "" 
+  },
+  { 
+    "name": "Scrum", 
+    "ring": "adopt", 
+    "quadrant": "methodologies", 
+    "isNew": "FALSE", 
+    "description": "" 
   }
 ];
